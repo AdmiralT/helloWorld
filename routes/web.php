e33a96f64549e39bae98f76ecc6223a8f6243cf8
Cian Byrne <N00172857@iadt.ie>
2019-09-24 14:13:32 +0100

    My first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+# @Date:   2019-09-24T13:36:46+01:00
+# @Last modified time: 2019-09-24T13:54:01+01:00
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('helloworld', function() {
+  return '<h1>Hello World</h1>';
+});
+
+Route::get('cianbyrne', function() {
+  return view('cianbyrne');
+});
